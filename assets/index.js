4d14edd2641eaa8d5ad8a1b9affc0b5817160388
askan-ken <45626138+askan-ken@users.noreply.github.com>
2022-11-22 16:59:46 +0700

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+import pertemuan1 from './pertemuan1.jpeg'
+import pertemuan2 from './pertemuan2.jpeg'
+import pertemuan3 from './pertemuan3.jpg'
+import card1 from './card1.jpeg'
+import card2 from './card2.jpeg'
+import card3 from './card3.jpg'
+
+
+export {
+    pertemuan1,
+    pertemuan2,
+    pertemuan3,
+    card1,
+    card2,
+    card3,
+}
